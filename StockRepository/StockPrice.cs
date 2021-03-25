3a0703b1b2fde012ff84286c171a45ec8c81a6ab
paul <paul@meso-tek.com>
2021-03-26 00:54:15 +0800

    first add

---
+++ StockRepository/StockPrice.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace StockRepository
+{
+    public class StockPrice
+    {
+
+        public double getStockReportPrice(in int stockId, string date, string field_name)
+        {
+            var twseStockDayReportRequestUrl = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date={0}&stockNo={1}&_=1615738116281";
+            
+            var queryDate = DateTime.ParseExact(date, "yyyyMMdd", null);
+
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+
+            var queryDateStr = string.Empty; 
+            queryDateStr = queryDate.ToString("yyy/MM/dd",culture);
+
+            RestSharp.RestClient rc = new RestClient(String.Format(twseStockDayReportRequestUrl, date, stockId));
+            var rcResponse = rc.Get(new RestRequest());
+
+            DayTradeReportResponse response = JsonConvert.DeserializeObject<DayTradeReportResponse>(rcResponse.Content);
+
+            if (response.stat != "OK") throw new Exception("回應錯誤");
+            if (response.fields.Length == 0) throw new Exception("fields回應錯誤");
+            if (response.data.Length == 0) throw new Exception("data回應錯誤");
+
+
+            var dateIndex = Array.IndexOf(response.fields, "日期");
+            var dataIndex = Array.IndexOf(response.fields, field_name);
+            if (dateIndex < 0) throw new Exception("日期欄位不存在");
+            if (dataIndex < 0) throw new Exception(field_name + "欄位不存在");
+
+            var findDataStr = string.Empty;
+            foreach (var dataRow in response.data)
+            {
+                if (dataRow[dateIndex] == queryDateStr)
+                {
+                    var data = dataRow[dataIndex];
+                    if (data.Length > 0)
+                    {
+                        findDataStr = data;
+                        break;
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(findDataStr))
+                throw new Exception("找不到對應日期的資料");
+
+            double stockClosingPrice = -1;
+            double.TryParse(findDataStr, out stockClosingPrice);
+
+            return stockClosingPrice;
+        }
+    }
+
+    public class DayTradeReportResponse
+    {
+        public string stat { get; set; }
+
+        public string date { get; set; }
+        public string[] fields { get; set; }
+        public string[][] data { get; set; }
+    }
+}
---
+++ StockRepositoryTests/StockPriceTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockRepository.Tests
+{
+    [TestClass()]
+    public class StockPriceTests
+    {
+        [TestMethod]
+        public void GetStockClosePriceTest()
+        {
+            StockPrice sp = new StockPrice();
+            var stockId = 2330;
+            var date = "20210304";
+            
+            var price = sp.getStockReportPrice(stockId, date, "收盤價");
+            Assert.AreEqual(price, 601);
+        }
+
+        [TestMethod]
+        public void GetStockOpenPriceTest()
+        {
+            StockPrice sp = new StockPrice();
+            var stockId = 2330;
+            var date = "20210304";
+            
+            var price = sp.getStockReportPrice(stockId, date, "開盤價");
+            Assert.AreEqual(price, 609);
+        }
+
+        [TestMethod]
+        public void GetStockNotTradeDatePriceTest()
+        {
+            //假日
+            StockPrice sp = new StockPrice();
+            var stockId = 2330;
+            var date = "20210314";
+            Assert.ThrowsException<Exception>(() => sp.getStockReportPrice(stockId, date, "開盤價"), "找不到對應日期的資料");
+        }
+    }
+}
